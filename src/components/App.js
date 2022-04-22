7f1ca2e40f7f5231f4dd957e69cd110453d7e93c
Craig Ford <craig_ford@icloud.com>
2022-04-21 20:00:48 -0400

    commit tree 16420ebb4a7acee37ceed016a6afbee2ad3e32ea

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [isLight, setIsLight] = useState("false");
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  function handleClick(event){
+    setIsLight(!isLight)
+  }  
 
+  const appClass = isLight ? "App dark" : "App light"
+  console.log('appClass = ', appClass)
+  
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{isLight ? "Light Mode" : "Dark Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState("false");
+  const [buttonText, setButtonText] = useState("Add to Cart");
+
+  const handleClick = (event) => {
+     
+    setInCart(!inCart)
+
+    let newButtonText = event.target.innerText 
+
+    if (newButtonText === "Add to Cart") {
+        newButtonText = "Remove From Cart";
+    } else {
+        newButtonText = "Add to Cart"
+    }
+
+    setButtonText(newButtonText);
+      
+  }  
+
   return (
-    <li className="">
+    <li className={`${inCart ? "" : "in-cart"}`}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{buttonText}</button>
     </li>
-  );
+  )
 }
 
 export default Item;
