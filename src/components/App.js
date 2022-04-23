302c7030e4730845ad170334f3ac2cd10dd4473a
Craig Ford <craig_ford@icloud.com>
2022-04-23 18:29:33 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,17 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  const [isLight, setIsLight] = useState("false");
+  const [isLight, setIsLight] = useState(false);
 
   function handleClick(event){
     setIsLight(!isLight)
   }  
 
   return (
-    <div className={isLight ? "App light" : "App dark"}>
+    <div className={isLight ? "App dark" : "App light"}>
       <header>
         <h2>Shopster</h2>
-        <button onClick={handleClick}>{isLight ? "Light Mode" : "Dark Mode"}</button>
+        <button onClick={handleClick}>{isLight ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
